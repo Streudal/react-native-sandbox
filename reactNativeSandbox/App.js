bb300701a7e4098f4e8ad6a2866d4b7aa34d802f
Tyler <streuta@cdmsmith.com>
2020-10-25 18:15:03 -0500

    Messing around with drawer and buttons.

--- reactNativeSandbox/App.js
+++ reactNativeSandbox/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { StyleSheet, SafeAreaView, ScrollView, Button, View, Text, LogBox  } from 'react-native';
+import { StyleSheet, SafeAreaView, ScrollView, Button, View, Text, LogBox, TouchableOpacity } from 'react-native';
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { FloatingAction } from 'react-native-floating-action';
@@ -18,26 +18,19 @@ function Feed({ navigation }) {
   );
 }
 
-function Notifications() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Notifications Screen</Text>
-    </View>
-  );
-}
-
 function CustomDrawerContent(props) {
   return (
     <DrawerContentScrollView {...props}>
       <DrawerItemList {...props} />
-      <DrawerItem
-        label="Close drawer"
-        onPress={() => props.navigation.closeDrawer()}
-      />
-      <DrawerItem
-        label="Toggle drawer"
-        onPress={() => props.navigation.toggleDrawer()}
-      />
+      <Text>Basemap Layers</Text>
+        <DrawerItem
+          label="Dark"
+          onPress={() => props.navigation.closeDrawer()}
+        />
+        <DrawerItem
+          label="Light"
+          onPress={() => props.navigation.closeDrawer()}
+        />
     </DrawerContentScrollView>
   );
 }
@@ -46,21 +39,11 @@ const Drawer = createDrawerNavigator();
 
 function MyDrawer() {
   return (
-    <Drawer.Navigator drawerContent={props => <CustomDrawerContent {...props} />}>
+    <Drawer.Navigator drawerPosition="right" overlayColor={0} drawerStyle={{ backgroundColor: '#c6cbef', width: 240 }} drawerContent={(props) => <CustomDrawerContent {...props} />}>
       <Drawer.Screen name="Feed" component={Feed} />
-      <Drawer.Screen name="Notifications" component={Notifications} />
     </Drawer.Navigator>
   );
 }
-
-const actions = [
-  {
-    text: "Accessibility",
-    icon: require("./src/navigation/images/add.png"),
-    name: "bt_accessibility",
-    position: 1
-  },
-];
 
 const styles = StyleSheet.create({
   container: {
@@ -72,11 +55,23 @@ const styles = StyleSheet.create({
     height: 22,
     color: 'white',
   },
+  mainContainer: {
+    marginTop: 20,
+  },
+  detailsContainer: {
+    backgroundColor: '#FFF',
+    marginTop: 10,
+  },
+  title: {
+    paddingLeft: 16,
+    color: '#979797',
+    textTransform: 'uppercase',
+  },
 });
 
 const App: () => React$Node = () => {
   useEffect(() => {
-      LogBox.ignoreLogs(['Animated: `useNativeDriver`']); // Ignores bug logs for Animations.
+    LogBox.ignoreLogs(['Animated: `useNativeDriver`']); // Ignores bug logs for Animations.
   }, [])
   return (
     <SafeAreaView style={styles.container}>
@@ -84,12 +79,6 @@ const App: () => React$Node = () => {
         <NavigationContainer>
           <MyDrawer />
         </NavigationContainer>
-        <FloatingAction
-          position="right"
-          actions={actions}
-          distanceToEdge={{ vertical: 1100, horizontal: 20 }}
-          onPressItem={(props) => props.navigation.toggleDrawer()}
-        />
         <ActionButton position="left" verticalOrientation="down" buttonColor="rgba(231,76,60,1)">
           <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
             <Icon name="md-create" style={styles.actionButtonIcon} />
