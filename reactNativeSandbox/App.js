f58cbcfffea303807ea3d4a759c87c6a54769324
Tyler <streuta@cdmsmith.com>
2020-10-25 16:09:05 -0500

    Added more button libraries to test out and found that mastermoo/react-native-action-button works the best.

--- reactNativeSandbox/App.js
+++ reactNativeSandbox/App.js
@@ -1,8 +1,12 @@
-import * as React from 'react';
-import { SafeAreaView, StyleSheet, ScrollView, Button, View, Text, StatusBar } from 'react-native';
-import { Header, LearnMoreLinks, Colors, DebugInstructions, ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
+import React, { useEffect } from 'react';
+import { StyleSheet, SafeAreaView, ScrollView, Button, View, Text, LogBox  } from 'react-native';
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import { FloatingAction } from 'react-native-floating-action';
+import Property from "./Property";
+import ActionButton from 'react-native-action-button';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 
 function Feed({ navigation }) {
   return (
@@ -49,13 +53,57 @@ function MyDrawer() {
   );
 }
 
+const actions = [
+  {
+    text: "Accessibility",
+    icon: require("./src/navigation/images/add.png"),
+    name: "bt_accessibility",
+    position: 1
+  },
+];
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f3f3f3"
+  },
+  actionButtonIcon: {
+    fontSize: 20,
+    height: 22,
+    color: 'white',
+  },
+});
+
 const App: () => React$Node = () => {
+  useEffect(() => {
+      LogBox.ignoreLogs(['Animated: `useNativeDriver`']); // Ignores bug logs for Animations.
+  }, [])
   return (
-    <>
-    <NavigationContainer>
-      <MyDrawer />
-    </NavigationContainer>
-    </>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <NavigationContainer>
+          <MyDrawer />
+        </NavigationContainer>
+        <FloatingAction
+          position="right"
+          actions={actions}
+          distanceToEdge={{ vertical: 1100, horizontal: 20 }}
+          onPressItem={(props) => props.navigation.toggleDrawer()}
+        />
+        <ActionButton position="left" verticalOrientation="down" buttonColor="rgba(231,76,60,1)">
+          <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
+            <Icon name="md-create" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor='#3498db' title="Notifications" onPress={() => { }}>
+            <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item buttonColor='#1abc9c' title="All Tasks" onPress={() => { }}>
+            <Icon name="add" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+        </ActionButton>
+      </View>
+    </SafeAreaView>
+
   );
 };
 
